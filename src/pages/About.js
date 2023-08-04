ec65e6487e93f6af701fff5de2598b97cb51cfe1
Ivan <ivanezeh12@gmail.com>
2023-08-04 13:30:28 +0100

    four

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,38 +1,34 @@
-import filler from "../Resources/Joko.JPG"
+import filler from "../Resources/Joko.JPG";
 
 function AboutPage() {
   return (
     <div className="bigBody">
       <div className="AboutCont">
-        <section className="peter">
-          <div className="head">
-            <h1>ABOUT</h1>
+        <section className="bighead">
+          <div className="catch">
+            <h1>Description</h1>
+            <span className="aboutty">
+              This is supposed to be an in depth and insightfull dscription of
+              this website but seeing as the primary focus of creating this
+              website is developing my react abilities, I will not be writing
+              anything besides this.
+            </span>
           </div>
+        </section>
+        <section className="peter">
+
           <div className="floater">
             <div className="imageHolster" id="ho">
               <img alt="talk talk" src={filler} />
-              <div className="overlay">
-                <span className="big">ELEGANCE</span>
-                <span className="smo">Color shit and other shit.</span>
-              </div>
             </div>
             <div className="imageHolster" id="centerbaby">
               <img alt="talk talk" src={filler} />
-              <div className="overlay">
-                <span className="big">ELEGANCE</span>
-                <span className="smo">Color shit and other shit.</span>
-              </div>
             </div>
             <div className="imageHolster" id="ha">
               <img alt="talk talk" src={filler} />
-              <div className="overlay">
-                <span className="big">ELEGANCE</span>
-                <span className="smo">Color shit and other shit.</span>
-              </div>
             </div>
           </div>
         </section>
-        <section className="bighead"> doccy</section>
       </div>
     </div>
   );
